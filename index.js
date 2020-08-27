cf972cd11e9c488b73af317c91f1aa20431713dc
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-27 17:27:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Write your solution in this file!
 const driver = {};
+
+// updateDriverWithKeyAndValue(driver, key, value)
+    // 1) returns a driver with the original key value pairs and the new key value pair
+    // 2) it does not modify the original driver, but rather returns a clone with the new data
