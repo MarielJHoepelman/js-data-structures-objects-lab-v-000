65b6a87cc3cf2eadb3000de06cf2ec358589ea8a
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-27 17:34:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const driver = {};
 // updateDriverWithKeyAndValue(driver, key, value)
     // 1) returns a driver with the original key value pairs and the new key value pair
     // 2) it does not modify the original driver, but rather returns a clone with the new data
+
+    function updateDriverWithKeyAndValue(driver, key, value) {
+      const addDriver = { ...obj };
+      addDriver[key] = value;
+
+      return newObj;
+    }
