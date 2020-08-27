b7d635a02d7fc061e4b0a5b59633c359437fd0cb
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-27 17:36:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const driver = {};
     function updateDriverWithKeyAndValue(obj, key, value) {
       return Object.assign({}, obj, { [key]: value });
     }
+
+    function destructivelyUpdateDriverWithKeyAndValue (obj, key, value) {
+      obj[key] = value;
+
+      return obj;
+    }
